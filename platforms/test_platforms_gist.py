c7980ccfc610c2a6fbbfdcd15b41e8f8ce6103c6
kamran haider <kamranhaider.mb@gmail.com>
2017-05-05 13:10:19 -0400

    initial commit

--- platforms/test_platforms_gist.py
+++ platforms/test_platforms_gist.py
@@ -2,7 +2,7 @@ import sys
 import os
 #sys.path.append("/Users/kamranhaider/Dropbox/SSTMap/sstmap")
 
-from sstmap.site_water_analysis import SiteWaterAnalysis
+from sstmap.grid_water_analysis import GridWaterAnalysis
 
 platforms = ["amber", "charmm", "desmond", "gromacs", "namd", "openmm"]
 top_ext = [".prmtop", ".psf", ".pdb", ".gro", ".psf", ".parm7"]
@@ -10,7 +10,7 @@ trj_ext = [".nc", ".dcd", ".nc", ".xtc", ".dcd", ".nc"]
 supp_ext = [None, "toppar", "params.txt", "params.top", "toppar", None]
 
 for index, platform in enumerate(platforms):
-    if platform == "amber":
+    if platform == "charmm":
         print("Testing: %s" % platform)
         os.chdir(platform)
         top = "testcase" + top_ext[index]
@@ -19,13 +19,14 @@ for index, platform in enumerate(platforms):
         ligand = "ligand.pdb"
         s = 0
         n = 100
-        hsa = SiteWaterAnalysis(top, traj, start_frame=s, num_frames=n, 
+
+        gist = GridWaterAnalysis(top, traj, start_frame=s, num_frames=n, 
                             ligand_file=ligand, supporting_file=supp,
+                            grid_dimensions=[20.0, 20.0, 20.0],
                             prefix="testcase")
-        hsa.print_system_summary()
-        hsa.initialize_hydration_sites()
-        hsa.calculate_site_quantities()
-        hsa.write_calculation_summary()
-        hsa.write_data()
+        gist.print_system_summary()
+        gist.calculate_grid_quantities(num_frames=1000)
+        gist.write_data()
+        gist.generate_dx_files()
         os.chdir("../")
 
