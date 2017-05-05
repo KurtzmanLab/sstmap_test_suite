13f5e05f0c08b04c02b332b006896542a85f1ae7
kamran haider <kamranhaider.mb@gmail.com>
2017-05-05 15:18:12 -0400

    finalized hsa platform tests

--- platforms/test_platforms_hsa.py
+++ platforms/test_platforms_hsa.py
@@ -10,22 +10,21 @@ trj_ext = [".nc", ".dcd", ".nc", ".xtc", ".dcd", ".nc"]
 supp_ext = [None, "toppar", "params.txt", "params.top", "toppar", None]
 
 for index, platform in enumerate(platforms):
-    if platform == "amber":
-        print("Testing: %s" % platform)
-        os.chdir(platform)
-        top = "testcase" + top_ext[index]
-        traj = "md100ps" + trj_ext[index]
-        supp = supp_ext[index]
-        ligand = "ligand.pdb"
-        s = 0
-        n = 100
-        hsa = SiteWaterAnalysis(top, traj, start_frame=s, num_frames=n, 
-                            ligand_file=ligand, supporting_file=supp,
-                            prefix="testcase")
-        hsa.print_system_summary()
-        hsa.initialize_hydration_sites()
-        hsa.calculate_site_quantities()
-        hsa.write_calculation_summary()
-        hsa.write_data()
-        os.chdir("../")
+    print("Testing: %s" % platform)
+    os.chdir(platform)
+    top = "testcase" + top_ext[index]
+    traj = "md100ps" + trj_ext[index]
+    supp = supp_ext[index]
+    ligand = "ligand.pdb"
+    s = 0
+    n = 100
+    hsa = SiteWaterAnalysis(top, traj, start_frame=s, num_frames=n, 
+                        ligand_file=ligand, supporting_file=supp,
+                        prefix="testcase")
+    hsa.print_system_summary()
+    hsa.initialize_hydration_sites()
+    hsa.calculate_site_quantities()
+    hsa.write_calculation_summary()
+    hsa.write_data()
+    os.chdir("../")
 
